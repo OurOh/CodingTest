60f95e1a9032eb0bc53dc707b2ebc5fa6d90c6bd
OurOh <ahn980321@gmail.com>
2024-11-29 11:58:53 +0900

    1129 Day2-1
    1129 Day2-1

---
+++ src/Bronze5/BOJ1008.java
@@ -0,0 +1,12 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+        System.out.println(A/B);
+    }
+}
