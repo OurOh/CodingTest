8e61d77cb86c4f7df5d2c1c04505890983b56215
OurOh <ahn980321@gmail.com>
2024-12-10 16:35:10 +0900

    Day11 1210-2
    Day11 1210-2

--- src/Bronze5/BOJ11654.java
+++ src/Bronze5/BOJ11654.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class BOJ11654 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        char input =  sc.next().charAt(0);
-
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        
         System.out.println((int)input);
     }
 }
+/* 자바에서 입력은 항상 String으로 받는다 */
+//next()는 단어 단위로 입력을 받아야 할 때 적합하고, nextLine()은 줄 단위로 전체를 받아야 할 때 적합합니다.
