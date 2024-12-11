a298b4e29a459f350d31b490750492f40490daa4
OurOh <ahn980321@gmail.com>
2024-12-11 12:11:02 +0900

    Day12 1211-1
    Day12 1211-1

---
+++ src/Bronze5/BOJ10951.java
@@ -0,0 +1,17 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A;
+        int B;
+        while (sc.hasNext()) {
+            A = sc.nextInt();
+            B = sc.nextInt();
+            System.out.println(A + B);
+
+        }
+    }
+}
---
+++ src/Bronze5/BOJ18108.java
@@ -0,0 +1,11 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        System.out.println(Y-543);
+    }
+}
