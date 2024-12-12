363fe2aa9afacff06117eb3dac8538674a660fe8
OurOh <ahn980321@gmail.com>
2024-12-12 23:07:28 +0900

    Day13 1212

---
+++ src/Bronze5/BOJ11718.java
@@ -0,0 +1,17 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+
+        }
+    }
+}
+
+// next는 단어 단위, nextLine은 한 줄 전체
