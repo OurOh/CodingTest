ff7a990d4d0922827224ea92b9e7ccf885e6bbd1
OurOh <ahn980321@gmail.com>
2024-12-13 17:47:33 +0900

    Day14 1213-1
    Day14 1213-1

---
+++ src/Bronze5/BOJ3003.java
@@ -0,0 +1,20 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ3003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] defaultPieces = {1, 1, 2, 2, 2, 8};
+        int[] inputPieces = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+            inputPieces[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(defaultPieces[i] - inputPieces[i] + " ");
+        }
+    }
+
+}
