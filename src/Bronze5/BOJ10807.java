e1626d30f49ecc4ea1ba32145b001edab0481d65
OurOh <ahn980321@gmail.com>
2024-12-15 18:21:58 +0900

    Day15 1215-2
    Day15 1215-2

--- src/Bronze5/BOJ10807.java
+++ src/Bronze5/BOJ10807.java
@@ -5,14 +5,24 @@ import java.util.Scanner;
 public class BOJ10807 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
-        int [] Y = new int[N];
-        int V = sc.nextInt();
+        int[] inputs = new int[N];
 
         for (int i = 0; i < N; i++) {
-            Y [i] = sc.nextInt();
+            inputs[i] = sc.nextInt();
         }
 
+        int V = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (inputs[i] == V) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
 
     }
 }
