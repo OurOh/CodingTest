bfe82b6f7586fddced89885ad1571ba00b006a0e
OurOh <ahn980321@gmail.com>
2024-12-21 23:31:36 +0900

    Day19 1221
    Day19 1221

---
+++ src/Bronze5/BOJ5597.java
@@ -0,0 +1,24 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class BOJ5597 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean [] submitted = new boolean[30];
+
+        for (int i = 0; i < 28; i++) {
+            int studentNumber = sc.nextInt();
+            submitted[studentNumber - 1] = true;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if (!submitted[i]) {
+                System.out.println(i+1);
+            }
+        }
+
+
+    }
+}
